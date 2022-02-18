f88cf8e1ae2218db0fe5662e019d12a8a17c5e25
Justin Hammel <justinhammel62@gmail.com>
2022-02-18 11:12:05 -0800

    AB#672, location now displayed on map

--- src/main/java/com/ChildrenOfSummer/SummerVacation/Util/MapLocation.java
+++ src/main/java/com/ChildrenOfSummer/SummerVacation/Util/MapLocation.java
@@ -1,7 +1,13 @@
 package com.ChildrenOfSummer.SummerVacation.Util;
 
+import org.json.simple.JSONObject;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -12,29 +18,50 @@ import java.util.Map;
  */
 
 public class MapLocation extends Canvas {
+    Toolkit t = Toolkit.getDefaultToolkit();
+
     // Map<String, ArrayList<Integer>> mapCoordinates = JsonHandler.jsonMapCoordinates("map_coordinates_test.json", "config");
     // Alternative when using ArrayList of String instead of trying to start with numbers in the JSON
     Map<String, ArrayList<String>> mapCoordinates = JsonHandler.jsonToMapStringList("map_coordinates.json", "config");
 
     public void paint(Graphics map) {
-        Toolkit t = Toolkit.getDefaultToolkit();
+        // Pull location from save data
+        JSONObject saveData = JsonHandler.loadGame();
+        String currentLocation = (String) saveData.get("location");
+        System.out.println(currentLocation);
+
+        // get the 2 images for map and marker
         Image image1 = t.getImage("./Assets/map/map.png");
         Image image2 = t.getImage("./Assets/map/marker.png");
+        // drawing just the map
         map.drawImage(image1, 0, 0, this);
         // for (Map.Entry<String, ArrayList<Integer>> entry : mapCoordinates.entrySet()) {
         for (Map.Entry<String, ArrayList<String>> entry : mapCoordinates.entrySet()) {
-            // Strange thing here, when starting with strings only need the parseInt(using Map<String, ArrayList<String>)
-            // When starting with Map<String, ArrayList<Integer>> somehow values come as longs.
-            // Longs are really difficult to get back to ints requiring 2 casts like below.
-            // int x = Integer.parseInt(String.valueOf(entry.getValue().get(0)));
-            // int y = Integer.parseInt(String.valueOf(entry.getValue().get(1)));
-
-            int x = Integer.parseInt(entry.getValue().get(0));
-            int y = Integer.parseInt(entry.getValue().get(1));
-            // right now this just prints marker at all coordinates in the json file
-            map.drawImage(image2, x, y, this);
+            if(entry.getKey().equalsIgnoreCase(currentLocation)) {
+                // Strange thing here, when starting with strings only need the parseInt(using Map<String, ArrayList<String>)
+                // When starting with Map<String, ArrayList<Integer>> somehow values come as longs.
+                // Longs are really difficult to get back to ints requiring 2 casts like below.
+                // int x = Integer.parseInt(String.valueOf(entry.getValue().get(0)));
+                // int y = Integer.parseInt(String.valueOf(entry.getValue().get(1)));
+                int x = Integer.parseInt(entry.getValue().get(0));
+                int y = Integer.parseInt(entry.getValue().get(1));
+                // this should print marker at matched coordinates in the json file
+                map.drawImage(image2, x, y, this);
+                break;
+            }
         }
+    }
 
+    public BufferedImage getMarkerImage() {
+        BufferedImage image = null;
+        try {
+            URL url = new URL("file:./Assets/map/marker.png");
+            image = ImageIO.read(url);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
 }
